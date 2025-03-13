255aa5fb85fbdbedd100ed1928dcd4aa9daad884
tanmayg1313 <143797651+tanmayg1313@users.noreply.github.com>
2025-03-13 15:59:24 +0530

    Updated hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void)
 {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "PES1UG22CS643n";
+  cout << "PES1UG22CS643\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
